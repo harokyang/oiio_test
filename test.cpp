0670db3d123e5fa2679b76dcb9b4ebf89bf8aa7d
Harok <harokyang@foxmail.com>
2019-09-20 13:57:57 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,20 @@
+﻿#include <OpenImageIO/imageio.h>
+
+int main()
+{
+    auto input = OIIO::ImageInput::open("file_path");
+
+    auto const& image_spec = input->spec();
+    auto read_format = image_spec.format;
+
+    auto channel_count = (uint32_t)image_spec.nchannels;
+    if (channel_count == 3)
+        channel_count = 4;
+
+    std::vector<unsigned char> pixels(1000);
+
+    input->read_image(read_format, pixels.data(), 4);
+    input->close();
+
+    return 0;
+}
